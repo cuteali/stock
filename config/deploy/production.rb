e16b7de4edc8fef49892c98167221891e71cfcd8
zhangkun <313822030@qq.com>
2016-03-22 12:40:49 +0800

    fixed products select category_id.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,12 +4,27 @@ set :stage, :production
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'yaohuo.la', user: 'ubuntu', roles: %w{app db web}, primary: true
+# server 'yaohuo.la', user: 'ubuntu', roles: %w{app db web}, primary: true
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
+# chef_role :db,      'role:appserver AND chef_environment:prod',  limit: 1, primary: true
+# chef_role :app,     'role:appserver AND chef_environment:prod'
+# chef_role :web,     'role:appserver AND chef_environment:prod'
+# chef_role :worker,  'role:worker AND chef_environment:prod'
+
 set :user,          'ubuntu'
 set :rails_env,     'production'
+
+# connection_options = {
+#     port:           22,
+#     forward_agent:  true,
+#     user:           'ubuntu',
+#     keys:           %w(~/.ssh/id_rsa),
+#     auth_methods:   %w(publickey)
+# }
+
+# set :ssh_options,   connection_options
 
 # role-based syntax
 # ==================
@@ -43,21 +58,21 @@ set :rails_env,     'production'
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+ set :ssh_options, {
+   keys: %w(~/.ssh/id_rsa),
+   forward_agent: false,
+   auth_methods: %w(password)
+ }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+server 'yaohuo.la',
+  user: 'ubuntu',
+  roles: %w{web app},
+  ssh_options: {
+    user: 'user_name', # overrides user setting above
+    keys: %w(~/.ssh/id_rsa),
+    forward_agent: false,
+    auth_methods: %w(publickey password)
+    # password: 'please use keys'
+  }
