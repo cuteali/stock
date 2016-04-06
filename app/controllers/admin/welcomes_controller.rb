6b7a005e7f8199748e05d3faf474f6755f050a57
zhangkun <313822030@qq.com>
2016-04-06 13:15:37 +0800

    fixed something.

--- app/controllers/admin/welcomes_controller.rb
+++ app/controllers/admin/welcomes_controller.rb
@@ -1,6 +1,13 @@
 class Admin::WelcomesController < Admin::BaseController
-
+  skip_before_filter :filter_current_member
 
   def index
+    if current_member && current_member.is_a?(Member)
+      redirect_to admin_products_url
+    elsif request.referrer =~ /admin/i || request.referrer =~ /members/i
+      redirect_to admin_url
+    else
+      redirect_to app_download_service_agreement_index_url
+    end
   end
 end
